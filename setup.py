545945243e374837faa8f7e85cc7b1ec7587e247
Benjamin Turner <benturn@gmail.com>
2012-03-13 21:53:55 -0700

    packaging

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='bturner_django_utils',
+    version='0.1',
+    author=u'Benjamin Turner',
+    author_email='benturn@gmail.com',
+    packages=['bturner_django_utils'],
+    url='https://github.com/blturner/bturner_django_utils',
+    license='BSD licence, see LICENCE.txt',
+    description='Comomonly used patterns in my django development.',
+    long_description=open('README').read(),
+    zip_safe=False,
+)
